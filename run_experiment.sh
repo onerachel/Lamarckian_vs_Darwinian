10dd55c2d02f1f20b14cd1e4cc4f905d1c099a2f
onerachel <2206266@gmail.com>
2022-12-26 14:46:42 +0100

    add run_experiment

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-experiment_optimize=jlo/ea_fixedbody_cppn_openaies/optimize.py
-experiment_name=openaies_1
+experiment_optimize=darwinian_evolution/optimize.py
+experiment_name=darwinian_rotation
 
-screen -d -m -S "${experiment_name}" -L -Logfile "./${experiment_name}.log" nice -n19 python3 "${experiment_optimize}"
-
-
+for num in {1..2}
+do
+  screen -d -m -S "${experiment_name}" -L -Logfile "./${experiment_name}.log" nice -n19 python3 "${experiment_optimize}"
+done
