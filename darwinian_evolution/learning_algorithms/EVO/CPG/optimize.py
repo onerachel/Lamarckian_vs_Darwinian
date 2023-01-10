ff37f247199bced570e057d929a55b750043cc01
onerachel <2206266@gmail.com>
2023-01-10 17:12:06 +0100

    save fitnesses before and after learning

--- darwinian_evolution/learning_algorithms/EVO/CPG/optimize.py
+++ darwinian_evolution/learning_algorithms/EVO/CPG/optimize.py
@@ -7,7 +7,7 @@ from random import Random
 from .optimizer import Optimizer
 from revolve2.core.database import open_async_database_sqlite
 from revolve2.core.optimization import DbId
-from standard_resources.revolve2.standard_resources import modular_robots
+from revolve2.standard_resources import modular_robots
 from .revde_optimizer import DbRevDEOptimizerBestIndividual
 from revolve2.core.database.serializers import Ndarray1xnSerializer
 from revolve2.core.modular_robot.brains import (
